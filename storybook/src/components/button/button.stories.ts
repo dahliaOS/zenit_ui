65c0a8bb28015e1b264b14eaf0e33e7820f01918
Blake Leonard <me@blakes.dev>
2022-11-15 02:57:29 +0000

    chore: document buttons better
    Every button starts off life as a little nub, which grows and grows
    until one day, it'll take up your entire monitor, one big button,
    just think about it.
    
    Signed-off-by: Blake Leonard <me@blakes.dev>

--- storybook/src/components/button/button.stories.ts
+++ storybook/src/components/button/button.stories.ts
@@ -5,7 +5,8 @@ import { ArgTypes } from '@storybook/web-components';
 import results from '../../../.jest-test-results.json';
 import { withTests } from '@storybook/addon-jest';
 
-//import docs from './my-component.docs.mdx';
+//@ts-ignore
+import docs from './button.docs.mdx';
 import { Button } from './button';
 
 export default {
@@ -14,8 +15,8 @@ export default {
   decorators: [withTests({ results })],
   parameters: {
     docs: {
-      description: "A button is an interactive component which, when tapped or clicked, performs some action."
-      //page: docs
+      description: "A button is an interactive component which, when tapped or clicked, performs some action.",
+      page: docs
     },
     actions: {
       handles: ['click'],
@@ -113,7 +114,7 @@ Rounded.argTypes = {
 }
 
 export const Icon = ({primary, disabled}) => html`<zenit-button ?primary=${primary} icon ?disabled=${disabled}>
-  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M20.42 4.58a5.4 5.4 0 0 0-7.65 0l-.77.78-.77-.78a5.4 5.4 0 0 0-7.65 0C1.46 6.7 1.33 10.28 4 13l8 8 8-8c2.67-2.72 2.54-6.3.42-8.42z"></path><path d="M12 5.36 8.87 8.5a2.13 2.13 0 0 0 0 3h0a2.13 2.13 0 0 0 3 0l2.26-2.21a3 3 0 0 1 4.22 0l2.4 2.4"></path><path d="m18 15-2-2"></path><path d="m15 18-2-2"></path></svg>
+  <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M20.42 4.58a5.4 5.4 0 0 0-7.65 0l-.77.78-.77-.78a5.4 5.4 0 0 0-7.65 0C1.46 6.7 1.33 10.28 4 13l8 8 8-8c2.67-2.72 2.54-6.3.42-8.42z"></path><path d="M12 5.36 8.87 8.5a2.13 2.13 0 0 0 0 3h0a2.13 2.13 0 0 0 3 0l2.26-2.21a3 3 0 0 1 4.22 0l2.4 2.4"></path><path d="m18 15-2-2"></path><path d="m15 18-2-2"></path></svg>
 </zenit-button>`
 Icon.argTypes = {
   primary: {
@@ -128,3 +129,5 @@ export const PrimaryDisabled = ({label, disabled}) => html`<zenit-button primary
 PrimaryDisabled.args = {
   disabled: true
 }
+
+//export const DocsDemo = () => html`<zenit-button primary>Primary</zenit-button>`
